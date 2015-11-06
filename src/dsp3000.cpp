bfdda7798c00cc07b65acfa376b9611f0833d0fa
sbir <sbir@sbir-big.domain.local>
2015-11-06 08:53:06 -0700

    debugging some changes

--- src/dsp3000.cpp
+++ src/dsp3000.cpp
@@ -48,6 +48,35 @@ const float PI=3.14159265359;
 
 using namespace std;
 
+bool configure_dsp3000(cereal::CerealPort *const device)
+{
+	bool output = true;
+
+	// Start by zeroing the sensor.  Write three times, to ensure it is received (according to datasheet)
+	ROS_INFO("Zeroing the DSP-3000.");
+	try{ device->write("ZZZ", 3); }
+	catch(cereal::TimeoutException& e)
+	{
+		ROS_ERROR("Unable to communicate with DSP-3000 device.");
+		output = false;
+	}
+	//ros::Duration(0.1).sleep();
+
+	if (output)
+	{
+  		// Set to "Rate" output.  R=Rate, A=Incremental Angle, P=Integrated Angle
+		ROS_INFO("Configuring for Rate output.");
+		try{ device->write("RRR", 3); }
+		catch(cereal::TimeoutException& e)
+		{
+			ROS_ERROR("Unable to communicate with DSP-3000 device.");
+		}
+	}
+	//ros::Duration(0.1).sleep();
+
+  return output;
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "dsp3000");
@@ -60,15 +89,13 @@ int main(int argc, char **argv)
   ros::param::param<std::string>("~port", port_name, "/dev/ttyUSB0");
 
   // Define the publisher topic name
-  ros::Publisher dsp3000_pub = n.advertise<std_msgs::Float32>("dsp3000", 1000);
-
-  //ros::Rate loop_rate(100);
+  ros::Publisher dsp3000_pub = n.advertise<std_msgs::Float32>("dsp3000", 10);
 
   cereal::CerealPort device;
 
-  char reply[64];
   float rotate;
-  int valid;
+  bool valid;
+  ROS_DEBUG("STARTED");
 
   // Open a port as defined in the launch file.  The DSP-3000 baud rate is 38400.
   try{ device.open(port_name.c_str(), 38400); }
@@ -77,65 +104,54 @@ int main(int argc, char **argv)
       ROS_FATAL("Failed to open the serial port!!!");
       ROS_BREAK();
   }
-  ROS_INFO("The serial port is opened.");
+  ROS_INFO("The serial port named \"%s\" is opened.", port_name.c_str());
 
-  // Configure the DSP-3000.
-  // Start by zeroing the sensor.  Write three times, to ensure it is received (according to datasheet)
-  ROS_INFO("Zeroing the DSP-3000.");
-  try{ device.write("ZZZ", 3); }
-  catch(cereal::TimeoutException& e)
-    {
-      ROS_ERROR("Unable to communicate with DSP-3000 device.");
-    }
-
-  // Set to "Rate" output.  R=Rate, A=Incremental Angle, P=Integrated Angle
-  ROS_INFO("Configuring for Rate output.");
-  try{ device.write("RRR", 3); }
-  catch(cereal::TimeoutException& e)
-    {
-      ROS_ERROR("Unable to communicate with DSP-3000 device.");
-    }
-
+  configure_dsp3000(&device);
+  static const int TEMP_BUFFER_SIZE = 64;
+  char temp_buffer[TEMP_BUFFER_SIZE];
   while (ros::ok())
   {
-    
     // Get the reply, the last value is the timeout in ms
-    try{ device.readLine(reply, TIMEOUT); }
+    try{ device.readLine(temp_buffer, TEMP_BUFFER_SIZE, TIMEOUT); }
     catch(cereal::TimeoutException& e)
       {
         ROS_ERROR("Unable to communicate with DSP-3000 device.");
       }
 
-    string str(reply);
-    string buf; // A buffer string
+    string str(temp_buffer); // A buffer string
     stringstream ss(str); // Insert the string into a stream
     vector<string> tokens; // Create a vector to hold the words
 
-    while (ss >> buf)
-      tokens.push_back(buf);
+    string ss_buf;
+    while (ss >> ss_buf)
+      tokens.push_back(ss_buf);
 
-    // Extract the data we want from the string vector.
-    rotate = atof(tokens[0].c_str());
-    valid = atoi(tokens[1].c_str());
+    if (2 == tokens.size())
+    {
+    	// Extract the data we want from the string vector.
+    	rotate = atof(tokens[0].c_str());
+    	valid = (1 == atoi(tokens[1].c_str()) ? true : false);
 
-    // Used for debugging.  The DSP-3000 outputs a "valid" flag as long as the
-    //data being output is OK.
-    ROS_DEBUG("Raw DSP-3000 Output: %f", rotate);
-    if (valid==1)
-         ROS_DEBUG("Data is valid");
-    
+    	// Used for debugging.  The DSP-3000 outputs a "valid" flag as long as the
+    	//data being output is OK.
+    	ROS_DEBUG("Raw DSP-3000 Output: %f", rotate);
+    	if (valid)
+    	     ROS_DEBUG("Data is valid");
+    	
 
-    //Declare the sensor message
-    std_msgs::Float32 dsp_out;
-    dsp_out.data = (rotate * PI) / 180;
+    	//Declare the sensor message
+    	std_msgs::Float32 dsp_out;
+    	dsp_out.data = (rotate * PI) / 180;
 
-    //Publish the joint state message
-    dsp3000_pub.publish(dsp_out);
+    	//Publish the joint state message
+    	dsp3000_pub.publish(dsp_out);
+    }
+    else
+    {
+	ROS_WARN("bad data. Received data of length %i", static_cast<int>(tokens.size()));
+    }
 
     ros::spinOnce();
-
-    //loop_rate.sleep();
-
   }
 
   return 0;
